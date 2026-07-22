2b74a395b49e34412e8e66a4642aeffea44f54c9
bench jay <bsumalinog84951@liceo.edu.ph>
2026-07-22 10:56:43 +0800

    profile card

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Palette
+val PrimaryLight = Color(0xFF771C1B)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFE9C9C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Palette
+val PrimaryDark = Color(0xFFE0A3A0)
+val OnPrimaryDark = Color(0xFF511313)
+val PrimaryContainerDark = Color(0xFF651817)
