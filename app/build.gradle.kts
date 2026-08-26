6e89d1e1d25e67a9ab91651079b4313faf3d65e7
bench jay <bsumalinog84951@liceo.edu.ph>
2026-08-26 10:29:47 +0800

    Midterm exam: MyRecipeBook dish and recipe CRUD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.midtermexam"
     compileSdk {
         version = release(37)
     }
@@ -51,4 +51,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    // Navigation for Compose
+    implementation("androidx.navigation:navigation-compose:2.9.8")
+
+// ViewModel + safe state collection in Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
 }
